a431c2ebb3f8260c832879e323821198fdd5f87c
Carsten Klee <cKlee@users.noreply.github.com>
2017-05-11 13:44:01 +0200

    1.0.0

--- lib/MARC/Spec.pm
+++ lib/MARC/Spec.pm
@@ -5,7 +5,7 @@ use Moo;
 use MARC::Spec::Parser;
 use namespace::clean;
 
-our $VERSION = '0.1.4';
+our $VERSION = '1.0.0';
 
 has field => (
     is => 'rw',
--- lib/MARC/Spec/Subspec.pm
+++ lib/MARC/Spec/Subspec.pm
@@ -3,7 +3,7 @@ package MARC::Spec::Subspec;
 use Moo;
 use namespace::clean;
 
-our $VERSION = '0.1.4';
+our $VERSION = '1.0.0';
 
 has left => (
     is => 'rw',
